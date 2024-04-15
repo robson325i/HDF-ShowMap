15fc530561f25e776657c176d31c4f8f19bd4372
robson325i <robsonbromberg@gmail.com>
2024-04-15 13:03:56 -0300

    Adicionar arquivos de projeto.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace HDF_ShowMap
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+            btnOpenFile = new Button();
+            ttbFileName = new TextBox();
+            lblFileName = new Label();
+            SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            formsPlot1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            formsPlot1.DisplayScale = 1F;
+            formsPlot1.Location = new Point(12, 56);
+            formsPlot1.Name = "formsPlot1";
+            formsPlot1.Size = new Size(640, 480);
+            formsPlot1.TabIndex = 0;
+            // 
+            // btnOpenFile
+            // 
+            btnOpenFile.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnOpenFile.Location = new Point(579, 27);
+            btnOpenFile.Name = "btnOpenFile";
+            btnOpenFile.Size = new Size(75, 23);
+            btnOpenFile.TabIndex = 1;
+            btnOpenFile.Text = "Abrir...";
+            btnOpenFile.UseVisualStyleBackColor = true;
+            btnOpenFile.Click += btnOpenFile_Click;
+            // 
+            // ttbFileName
+            // 
+            ttbFileName.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            ttbFileName.Location = new Point(12, 27);
+            ttbFileName.Name = "ttbFileName";
+            ttbFileName.Size = new Size(561, 23);
+            ttbFileName.TabIndex = 2;
+            // 
+            // lblFileName
+            // 
+            lblFileName.AutoSize = true;
+            lblFileName.Location = new Point(12, 9);
+            lblFileName.Name = "lblFileName";
+            lblFileName.Size = new Size(52, 15);
+            lblFileName.TabIndex = 3;
+            lblFileName.Text = "Arquivo:";
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(666, 551);
+            Controls.Add(lblFileName);
+            Controls.Add(ttbFileName);
+            Controls.Add(btnOpenFile);
+            Controls.Add(formsPlot1);
+            MinimumSize = new Size(682, 590);
+            Name = "FormMain";
+            Text = "FormMain";
+            Load += FormMain_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ScottPlot.WinForms.FormsPlot formsPlot1;
+        private Button btnOpenFile;
+        private TextBox ttbFileName;
+        private Label lblFileName;
+    }
+}
---
+++ FormMain.cs
@@ -0,0 +1,102 @@
+﻿using HDF.PInvoke;
+using ScottPlot;
+using ScottPlot.Plottables;
+using System.Runtime.InteropServices;
+
+namespace HDF_ShowMap
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void ReadHDF(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                return;
+            }
+
+            H5.open();
+            long fileId = H5F.open(fileName, H5F.ACC_RDONLY); // Abrir arquivo para somente leitura
+            string dataSetName = "/" + Path.GetFileNameWithoutExtension(fileName);
+            long dataSetId = H5D.open(fileId, dataSetName);
+            long dataSpace = H5D.get_space(dataSetId);
+
+            int rank = H5S.get_simple_extent_ndims(dataSpace); // Quantidade de dimensões do dataset
+            ulong[] dims = new ulong[rank];
+            H5S.get_simple_extent_dims(dataSpace, dims, null);
+
+            // HeatMap data
+            double[,] data = new double[dims[0], dims[1]]; // invertido
+            Heatmap hm = formsPlot1.Plot.Add.Heatmap(data);
+            hm.FlipVertically = true; // Inverter eixo y
+
+            formsPlot1.Plot.Axes.AutoScale();
+            hm.Colormap = new ScottPlot.Colormaps.Grayscale(); // mapa de cores
+            var cb = formsPlot1.Plot.Add.ColorBar(hm); // colorbar
+
+
+            // Ler o dataset do HDF, linha por linha
+            _ = Task.Run(() =>
+            {
+                for (ulong y = 0; y < dims[0]; y++)
+                {
+                    // Definir Hyperslab
+                    ulong[] start = [y, 0, 0];
+                    ulong[] count = [1, dims[1], dims[2]]; // Selecionar uma linha (valor y)
+                    int s = H5S.select_hyperslab(dataSpace, H5S.seloper_t.SET, start, null, count, null);
+
+                    // Alocar espaço na memória para leitura dos dados
+                    long memSpace = H5S.create_simple(3, count, null);
+
+                    double[,,] hdfData = new double[1, dims[1], dims[2]]; // y, x, z
+                    GCHandle dataHandle = GCHandle.Alloc(hdfData, GCHandleType.Pinned);
+                    _ = H5D.read(dataSetId, H5T.NATIVE_DOUBLE, memSpace, dataSpace, H5P.DEFAULT, dataHandle.AddrOfPinnedObject());
+                    dataHandle.Free();
+                    _ = H5S.close(memSpace);
+
+                    // somar valores
+                    for (ulong x = 0; x < dims[1]; x++)
+                    {
+                        double sum = 0;
+
+                        for (ulong z = 0; z < dims[2]; z++)
+                        {
+                            sum += hdfData[0, x, z];
+                        }
+
+                        data[y, x] = sum;
+                    }
+                    hm.Update();
+                    Invoke(() => formsPlot1.Refresh());
+                }
+                hm.Smooth = true;
+
+                MessageBox.Show("Carregamento Finalizado");
+            });
+        }
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new()
+            {
+                Filter = "Hierarchical Data Format 5 (*.HDF5; *.H5)|*.hdf5;*.h5"
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                ttbFileName.Text = ofd.FileName;
+                ReadHDF(ofd.FileName);
+            }
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            formsPlot1.Plot.HideGrid();
+            formsPlot1.Plot.HideLegend();
+            formsPlot1.Plot.Layout.Frameless();
+        }
+    }
+}
