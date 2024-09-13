d6e8a20a9bf4b23ebe10dc1598ad953742ef6bd7
robson325i <robsonbromberg@gmail.com>
2024-09-13 14:38:28 -0300

    Selecionar Dataset de uma lista

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -32,21 +32,31 @@
             btnOpenFile = new Button();
             ttbFileName = new TextBox();
             lblFileName = new Label();
+            splitContainer1 = new SplitContainer();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            label1 = new Label();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // formsPlot1
             // 
-            formsPlot1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             formsPlot1.DisplayScale = 1F;
-            formsPlot1.Location = new Point(12, 56);
+            formsPlot1.Dock = DockStyle.Fill;
+            formsPlot1.Location = new Point(0, 0);
             formsPlot1.Name = "formsPlot1";
             formsPlot1.Size = new Size(640, 480);
-            formsPlot1.TabIndex = 0;
+            formsPlot1.TabIndex = 5;
             // 
             // btnOpenFile
             // 
             btnOpenFile.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            btnOpenFile.Location = new Point(579, 27);
+            btnOpenFile.Location = new Point(168, 27);
             btnOpenFile.Name = "btnOpenFile";
             btnOpenFile.Size = new Size(75, 23);
             btnOpenFile.TabIndex = 1;
@@ -59,8 +69,8 @@
             ttbFileName.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             ttbFileName.Location = new Point(12, 27);
             ttbFileName.Name = "ttbFileName";
-            ttbFileName.Size = new Size(561, 23);
-            ttbFileName.TabIndex = 2;
+            ttbFileName.Size = new Size(152, 23);
+            ttbFileName.TabIndex = 0;
             // 
             // lblFileName
             // 
@@ -71,21 +81,100 @@
             lblFileName.TabIndex = 3;
             lblFileName.Text = "Arquivo:";
             // 
+            // splitContainer1
+            // 
+            splitContainer1.BorderStyle = BorderStyle.FixedSingle;
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.FixedPanel = FixedPanel.Panel1;
+            splitContainer1.IsSplitterFixed = true;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(dataGridView1);
+            splitContainer1.Panel1.Controls.Add(label1);
+            splitContainer1.Panel1.Controls.Add(button3);
+            splitContainer1.Panel1.Controls.Add(ttbFileName);
+            splitContainer1.Panel1.Controls.Add(lblFileName);
+            splitContainer1.Panel1.Controls.Add(btnOpenFile);
+            splitContainer1.Panel1MinSize = 250;
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(formsPlot1);
+            splitContainer1.Size = new Size(896, 482);
+            splitContainer1.SplitterDistance = 250;
+            splitContainer1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.Raised;
+            dataGridView1.ClipboardCopyMode = DataGridViewClipboardCopyMode.Disable;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1 });
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.Location = new Point(12, 85);
+            dataGridView1.MultiSelect = false;
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Size = new Size(231, 349);
+            dataGridView1.TabIndex = 7;
+            // 
+            // Column1
+            // 
+            Column1.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column1.HeaderText = "";
+            Column1.Name = "Column1";
+            Column1.ReadOnly = true;
+            Column1.Resizable = DataGridViewTriState.False;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 67);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Datasets:";
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            button3.Location = new Point(12, 440);
+            button3.Name = "button3";
+            button3.Size = new Size(231, 29);
+            button3.TabIndex = 5;
+            button3.Text = "Plotar dataset selecionado";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(666, 551);
-            Controls.Add(lblFileName);
-            Controls.Add(ttbFileName);
-            Controls.Add(btnOpenFile);
-            Controls.Add(formsPlot1);
-            MinimumSize = new Size(682, 590);
+            ClientSize = new Size(896, 482);
+            Controls.Add(splitContainer1);
+            MinimumSize = new Size(912, 521);
             Name = "FormMain";
-            Text = "FormMain";
+            Text = "HDF-Show Map";
             Load += FormMain_Load;
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -94,5 +183,10 @@
         private Button btnOpenFile;
         private TextBox ttbFileName;
         private Label lblFileName;
+        private SplitContainer splitContainer1;
+        private Button button3;
+        private Label label1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
     }
 }
--- FormMain.cs
+++ FormMain.cs
@@ -1,20 +1,58 @@
 ﻿using HDF.PInvoke;
+using HDF5CSharp;
+using HDF5CSharp.DataTypes;
 using ScottPlot.Plottables;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace HDF_ShowMap
 {
     public partial class FormMain : Form
     {
+        private CancellationTokenSource cancellationTokenSource;
+        private CancellationToken cancellationToken;
+
         public FormMain()
         {
             InitializeComponent();
         }
-
-        private void ReadHDF(string fileName)
+        private void ShowDatasets(string fileName)
         {
             if (!File.Exists(fileName))
             {
+                throw new FileNotFoundException("Arquivo não encontrado.");
+            }
+
+            long fileId = H5F.open(fileName, H5F.ACC_RDONLY);
+            if (fileId < 0)
+            {
+                throw new FileLoadException("Falha ao abrir o arquivo.");
+            }
+
+            var elements = Hdf5.ReadFlatFileStructure(fileName);
+            if (elements.Count > 0)
+            {
+                dataGridView1.Rows.Clear();
+            }
+
+            foreach (Hdf5Element element in elements)
+            {
+                if (element.Type == Hdf5ElementType.Dataset)
+                {
+                    int i = dataGridView1.Rows.Add(element.Name);
+                    dataGridView1.Rows[i].Resizable = DataGridViewTriState.False;
+                    dataGridView1.Rows[i].HeaderCell = null;
+                }
+            }
+        }
+        private void ReadHDF(string fileName, string dataSetPath)
+        {
+            if (!File.Exists(fileName))
+            {
+                MessageBox.Show("Arquivo inválido ou nulo.",
+                    "Falha ao abrir dataset",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
                 return;
             }
 
@@ -24,8 +62,8 @@ namespace HDF_ShowMap
 
             _ = H5.open();
             long fileId = H5F.open(fileName, H5F.ACC_RDONLY); // Abrir arquivo para somente leitura
-            string dataSetName = "/XRF/" + Path.GetFileNameWithoutExtension(fileName);
-            long dataSetId = H5D.open(fileId, dataSetName);
+            //string dataSetPath = "/XRF/" + Path.GetFileNameWithoutExtension(fileName);
+            long dataSetId = H5D.open(fileId, dataSetPath);
             long dataSpace = H5D.get_space(dataSetId);
 
             int rank = H5S.get_simple_extent_ndims(dataSpace); // Quantidade de dimensões do dataset
@@ -40,44 +78,76 @@ namespace HDF_ShowMap
             formsPlot1.Plot.Axes.AutoScale();
 
             hm.Colormap = new ScottPlot.Colormaps.Viridis(); // mapa de cores
+            hm.Smooth = false;
             var cb = formsPlot1.Plot.Add.ColorBar(hm); // colorbar
 
-            // Ler o dataset do HDF, linha por linha
-            _ = Task.Run(() =>
+            int threadCount = Environment.ProcessorCount;
+            if (threadCount > 1)
             {
-                for (ulong y = 0; y < dims[1]; y++)
+                // Remover um thread para manter a thread da interface desocupada
+                threadCount--;
+            }
+
+            int rowsPerThread = (int)dims[1] / threadCount;
+            int remaingRows = (int)dims[1] - rowsPerThread * threadCount;
+
+            // Definindo o hyperslab
+            ulong[] start = [0, 0, 0];
+            ulong[] count = [dims[0], dims[1], dims[2]]; // Selecionar quantidade de valores
+            _ = H5S.select_hyperslab(dataSpace, H5S.seloper_t.SET, start, null, count, null);
+
+            // Alocar espaço na memória para leitura dos dados
+            long memSpace = H5S.create_simple(3, count, null);
+
+            // Ler o dataset do HDF, linha por linha
+            double[,,] hdfData = new double[dims[0], dims[1], dims[2]]; // x, y, z
+            GCHandle dataHandle = GCHandle.Alloc(hdfData, GCHandleType.Pinned);
+            _ = H5D.read(dataSetId, H5T.NATIVE_DOUBLE, memSpace, dataSpace, H5P.DEFAULT, dataHandle.AddrOfPinnedObject());
+            dataHandle.Free();
+            _ = H5S.close(memSpace);
+
+            Task[] tasks = new Task[threadCount];
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                ulong startRow = (ulong)(rowsPerThread * i);
+                ulong rowsCount = (ulong)(i + 1 == threadCount ? rowsPerThread + remaingRows : rowsPerThread);
+
+                // somar valores
+                tasks[i] = Task.Run(() =>
                 {
-                    // Definir Hyperslab
-                    ulong[] start = [0, y, 0];
-                    ulong[] count = [dims[0], 1, dims[2]]; // Selecionar uma linha (valor y)
-                    int s = H5S.select_hyperslab(dataSpace, H5S.seloper_t.SET, start, null, count, null);
-
-                    // Alocar espaço na memória para leitura dos dados
-                    long memSpace = H5S.create_simple(3, count, null);
-
-                    double[,,] hdfData = new double[dims[0], 1, dims[2]]; // x, y, z
-                    GCHandle dataHandle = GCHandle.Alloc(hdfData, GCHandleType.Pinned);
-                    _ = H5D.read(dataSetId, H5T.NATIVE_DOUBLE, memSpace, dataSpace, H5P.DEFAULT, dataHandle.AddrOfPinnedObject());
-                    dataHandle.Free();
-                    _ = H5S.close(memSpace);
-
-                    // somar valores
-                    for (ulong x = 0; x < dims[0]; x++)
+                    Debug.WriteLine($"Runnning Task n#: {i}.");
+                    for (ulong y = startRow; y < startRow + rowsCount; y++)
                     {
-                        double sum = 0;
-
-                        for (ulong z = 0; z < dims[2]; z++)
+                        if (cancellationToken.IsCancellationRequested)
                         {
-                            sum += hdfData[x, 0, z];
+                            Debug.WriteLine($"Task n#: {i} cancelled.");
+                            return;
                         }
-                        data[y, x] = sum;
-                    }
-                    hm.Update();
-                    Invoke(() => formsPlot1.Refresh());
-                }
-                hm.Smooth = false;
 
-                MessageBox.Show("Carregamento Finalizado");
+                        for (ulong x = 0; x < dims[0]; x++)
+                        {
+                            double sum = 0;
+
+                            for (ulong z = 0; z < dims[2]; z++)
+                            {
+                                sum += hdfData[x, y, z];
+                            }
+                            lock (data)
+                            {
+                                data[y, x] = sum;
+                            }
+                        }
+                    }
+                }, cancellationToken);
+                Thread.Sleep(50);
+            }
+            Task.WaitAll(tasks);
+
+            Invoke(() =>
+            {
+                hm.Update();
+                formsPlot1.Refresh();
             });
         }
         private void BtnOpenFile_Click(object sender, EventArgs e)
@@ -89,15 +159,44 @@ namespace HDF_ShowMap
 
             if (ofd.ShowDialog() == DialogResult.OK)
             {
+                cancellationTokenSource.Cancel();
+                cancellationTokenSource = new();
+                cancellationToken = cancellationTokenSource.Token;
                 ttbFileName.Text = ofd.FileName;
-                ReadHDF(ofd.FileName);
+                ShowDatasets(ttbFileName.Text);
             }
         }
         private void FormMain_Load(object sender, EventArgs e)
         {
+            cancellationTokenSource = new CancellationTokenSource();
+            cancellationToken = cancellationTokenSource.Token;
             formsPlot1.Plot.HideGrid();
             //formsPlot1.Plot.HideLegend();
             //formsPlot1.Plot.Layout.Frameless();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cancellationTokenSource.Cancel();
+            cancellationTokenSource = new();
+            cancellationToken = cancellationTokenSource.Token;
+
+            string? dataset;
+            try
+            {
+                dataset = dataGridView1.SelectedCells[0].FormattedValue.ToString();
+                if (dataset == null)
+                {
+                    return;
+                }
+            }
+            catch
+            {
+                return;
+            }
+            button3.Enabled = false;
+            ReadHDF(ttbFileName.Text, dataset);
+            button3.Enabled = true;
         }
     }
 }
